3a51db1593d2af6b06fa48906aa211fabbc114ef
zakariairungu <zacckirungu@gmail.com>
2024-02-18 11:39:55 +0300

    setting up

---
+++ src/main/java/com/orizon/dairyfarm/DairyFarmApplication.java
@@ -0,0 +1,13 @@
+package com.orizon.dairyfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orizon/dairyfarm/DairyFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orizon.dairyfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
